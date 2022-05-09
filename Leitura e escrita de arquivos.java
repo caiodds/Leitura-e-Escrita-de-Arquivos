da03133386778488489f4cf2299d5559e821a538
CaioLk PROGRAMADOR <99497978+caiodds@users.noreply.github.com>
2022-05-09 00:11:14 -0300

    Leitura e escrita de arquivos
    import java.util.ArrayList;
    import java.util.List;
    import java.util.Scanner;
    public class leitura {
        public static void main(String[] args) throws IOException {
            Scanner scanner = new Scanner(System.in);
            System.out.println("Deseja ler qual arquivo?");
            System.out.println("(1) Tarefas");
            System.out.println("(2) Registros");
            System.out.println("(3) Criar");
            String leitura = scanner.next();
    
                switch (leitura){
                    case "Tarefas":
                        Path arquivos = Paths.get("C:\\Users\\Nicoly Alves\\Desktop\\tarefas.txt");
                        List<String> linhass = Files.readAllLines(arquivos);
    
                        for (int i = 0; i < linhass.size();i++ ) {
                            String nome = linhass.get(i);
                            System.out.println("Tarefas "+nome);
                        }
                        break;
                    case "Registros":
                        Path arquivoss = Paths.get("C:\\Users\\Nicoly Alves\\Desktop\\Registro.txt");
                        List<String> linha = Files.readAllLines(arquivoss);
    
                        for (int i = 0; i < linha.size();i++ ) {
                            String nome = linha.get(i);
                            System.out.println("Registrados "+nome);
                        }
                        break;
                    case "Criar":
                        ArrayList<String> linhas = new ArrayList<>();
    
                        for (int i = 0; i < 5; i++){
                            System.out.print("Registro " + i + ": ");
                            String nome = scanner.nextLine();
    
                            linhas.add(nome);
                        }
    
                        Path arquivo = Paths.get("C:\\Users\\Nicoly Alves\\Desktop\\Arquivos.txt");
                        Files.write(arquivo, linhas);
    
                        scanner.close();
                        System.out.println("Fim...");
                }
        }
                }

---
+++ Leitura e escrita de arquivos.java
@@ -0,0 +1,57 @@
+# Leitura-e-Escrita-de-Arquivos
+Bem vindo
+
+
+
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class leitura {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Deseja ler qual arquivo?");
+        System.out.println("(1) Tarefas");
+        System.out.println("(2) Registros");
+        System.out.println("(3) Criar");
+        String leitura = scanner.next();
+
+            switch (leitura){
+                case "Tarefas":
+                    Path arquivos = Paths.get("C:\\Users\\Nicoly Alves\\Desktop\\tarefas.txt");
+                    List<String> linhass = Files.readAllLines(arquivos);
+
+                    for (int i = 0; i < linhass.size();i++ ) {
+                        String nome = linhass.get(i);
+                        System.out.println("Tarefas "+nome);
+                    }
+                    break;
+                case "Registros":
+                    Path arquivoss = Paths.get("C:\\Users\\Nicoly Alves\\Desktop\\Registro.txt");
+                    List<String> linha = Files.readAllLines(arquivoss);
+
+                    for (int i = 0; i < linha.size();i++ ) {
+                        String nome = linha.get(i);
+                        System.out.println("Registrados "+nome);
+                    }
+                    break;
+                case "Criar":
+                    ArrayList<String> linhas = new ArrayList<>();
+
+                    for (int i = 0; i < 5; i++){
+                        System.out.print("Registro " + i + ": ");
+                        String nome = scanner.nextLine();
+
+                        linhas.add(nome);
+                    }
+
+                    Path arquivo = Paths.get("C:\\Users\\Nicoly Alves\\Desktop\\Arquivos.txt");
+                    Files.write(arquivo, linhas);
+
+                    scanner.close();
+                    System.out.println("Fim...");
+            }
+    }
+            }
